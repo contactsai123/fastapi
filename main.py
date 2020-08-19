ba1869ad3d46a12df80c0ee6fc359424207accc2
Sai Sivasailem <contactsai123@gmail.com>
2020-08-19 15:43:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,19 +11,10 @@ def home():
 
 @app.get("/query")
 def func1():
-    return {[
-  {
+    return {
     "target":"upper_75", // The field being queried for
     "datapoints":[
       [622,1450754160000],  // Metric value as a float , unixtimestamp in milliseconds
       [365,1450754220000]
     ]
-  },
-  {
-    "target":"upper_90",
-    "datapoints":[
-      [861,1450754160000],
-      [767,1450754220000]
-    ]
   }
-]}
