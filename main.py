79c1f86fa3a3c6aaadc541e39d7db70838835521
Sai Sivasailem <contactsai123@gmail.com>
2020-08-29 12:58:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from flask import Flask
 from flask_cors import CORS
 
 app = FastAPI()
+CORS(app)
 
 #domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
 
 @app.get("/")
-@cross_origin()
 def home():
     return {"message":"Hello TutLinks.com"}
